8d985d3f95a84e4a424a549eba40f1ecddd5d4ca
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-24 07:07:27 +0330

    Add experience model

--- backend/experience/models.py
+++ backend/experience/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Experience(models.Model):
+    company = models.CharField(
+        max_length=100
+    )
+
+    position = models.CharField(
+        max_length=100
+    )
+
+    description = models.TextField()
+
+    start_date = models.DateField()
+
+    end_date = models.DateField(
+        blank=True,
+        null=True
+    )
+
+    current = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return f"{self.position} - {self.company}"
+
