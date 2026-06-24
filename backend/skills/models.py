186dc1c906a6fa62dba56ed7a713dd098ff69401
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-24 06:55:10 +0330

    Add skill model

--- backend/skills/models.py
+++ backend/skills/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Skills(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True
+    )
+    level = models.PositiveSmallIntegerField()
+    order = models.PositiveIntegerField(
+        default=0
+    )
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return self.name
