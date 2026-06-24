79235bbe6e0d43cb15454508d34e2ef789cad790
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-24 07:01:20 +0330

    Add project models

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(
+        max_length=100
+    )
+    description = models.TextField()
+    skills = models.ManyToManyField(
+        "skills.skill"
+    )
+    github_url = models.URLField()
+
+    live_url = models.URLField(
+        blank=True,
+        null=True
+    )
+
+    featured = models.BooleanField(
+        default=False
+    )
+
+    image = models.ImageField(
+        upload_to="projects/",
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateField()
+
+    def __str__(self):
+        return self.title
