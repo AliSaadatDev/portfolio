07803cbed0b1b3eb0d42ae330aaf6dfdb7cc0f6f
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-24 06:51:39 +0330

    Add profile model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    full_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    bio = models.TextField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    email = models.EmailField()
+    github_url = models.URLField()
+    linkedin_url = models.URLField(
+        blank=True,
+        null=True
+    )
+
+    avatar = models.ImageField(
+        upload_to="profile/"
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.full_name
