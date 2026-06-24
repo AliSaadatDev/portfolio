a4631b5d4c216e068c69b62bcc80696b7a679f98
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-24 07:40:37 +0330

    create all urls in main url config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profiles.views import ProfileViewSet
+from skills.views import SkillsViewSet
+from projects.views import ProjectsViewSet
+from experience.views import ExperienceViewSet
+
+router = DefaultRouter()
+router.register("profile", ProfileViewSet)
+router.register("skills", SkillsViewSet)
+router.register("projects", ProjectsViewSet)
+router.register("experience", ExperienceViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
