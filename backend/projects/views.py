932b6c56cc14d434da56127947673128b120dbd2
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-24 07:32:27 +0330

    Create view for projects

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+class ProfileViewSet(ReadOnlyModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
