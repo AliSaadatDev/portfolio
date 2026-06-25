4ee68e914c618fdba82a2b4f94cb722b7a54341c
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-25 14:02:20 +0330

    update front

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "http://backendalisad.vercel.app";
 
 export async function executeCommand(command) {
   const res = await fetch(`${BASE_URL}/terminal/execute/`, {
